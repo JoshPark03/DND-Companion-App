32508a42148f99af1d5a05beb779c4967e7fcbcb
j618p208 <joshjungpark@gmail.com>
2024-10-22 11:37:52 -0500

    App will start maximized and reduce to 70% of the screen when unmaximized

--- main.cpp
+++ main.cpp
@@ -7,12 +7,17 @@ int main(int argc, char ** argv) {
 	QApplication app (argc, argv);
 	// Main Window Object
 	QMainWindow mainWindow;
+	// setting window size to 70% of the screen
+	mainWindow.resize(app.primaryScreen()->availableGeometry().size()*.7);
+
 	// Sets to fullscreen (no minimize/close)
 	// mainWindow.setWindowState(Qt::WindowFullScreen);
 	// Sets to match window size
 	mainWindow.setWindowState(Qt::WindowMaximized);
+	
 	// Displays window
 	mainWindow.show();
 	// Runs the app
 	return app.exec();
 }
+
